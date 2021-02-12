3f009945f618748c3e153a1e1669a794eae6b608
william-eriksson-kling <55193729+william-eriksson-kling@users.noreply.github.com>
2021-02-12 11:24:51 +0100

    Kod

--- app.rb
+++ app.rb
@@ -2,3 +2,24 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+require_relative './model.rb'
+
+get('/day') do 
+    id = session[:id].to_i
+    db = SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
+    slim(:"day/index", locals:{todos:result})
+  end
+
+  get('/day/new') do
+    slim(:"day/new")
+  end
+
+  post('/day/new') do
+    title = params[:]
+    artist_id = params[:].to_i
+    db = SQLite3::Database.new("db/slutprojekt.db")
+    db.execute("INSERT INTO - VALUES (?,?)",)
+    redirect('/day')
+  end
