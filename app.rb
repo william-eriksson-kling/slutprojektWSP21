81f32e0550fe7229b8d96a365e7a5dfbb7af3b51
william-eriksson-kling <55193729+william-eriksson-kling@users.noreply.github.com>
2021-02-26 11:20:01 +0100

    Css + kod (lägga till dag och syssla)

--- app.rb
+++ app.rb
@@ -10,16 +10,25 @@ get('/day') do
     db.results_as_hash = true
     result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
     slim(:"day/index", locals:{todos:result})
-  end
+end
 
-  get('/day/new') do
-    slim(:"day/new")
-  end
+get('/day/new') do
+  slim(:"day/new")
+end
 
-  post('/day/new') do
-    title = params[:]
-    artist_id = params[:].to_i
-    db = SQLite3::Database.new("db/slutprojekt.db")
-    db.execute("INSERT INTO - VALUES (?,?)",)
-    redirect('/day')
-  end
+post('/day/new') do
+  day = params[:dag]
+  teamid = params[:team_id].to_i
+  db = SQLite3::Database.new("db/slutprojekt.db")
+  db.execute("INSERT INTO day VALUES (Date,Team_id)", day, teamid)
+  redirect('/day')
+end
+
+post('/todo/new') do
+  datum = params[:datum]
+  kategori = params[:kategori]
+  beskrivning = params[:beskrivning]
+  db = SQLite3::Database.new("db/slutprojekt.db")
+  db.execute("INSERT INTO to_do VALUES (Date,Type,Description )", datum, kategori, beskrivning)
+  redirect('/day')
+end
